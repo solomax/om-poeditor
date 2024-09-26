a65536583296862a7e83ca9b622ebc5595f81780
Maxim Solodovnik <solomax@apache.org>
2024-09-26 14:57:43 +0700

    Dependencies are updated

--- src/main/java/org/apache/solomax/App.java
+++ src/main/java/org/apache/solomax/App.java
@@ -24,9 +24,9 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.function.Function;
 
-import javax.ws.rs.core.Form;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.core.Form;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import org.apache.cxf.ext.logging.LoggingInInterceptor;
 import org.apache.cxf.ext.logging.LoggingOutInterceptor;
